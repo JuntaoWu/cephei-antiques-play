9202120dee84dbf63fcc6bfd1a497e5267b658bf
chenlunsen <563575103@qq.com>
2018-11-06 16:06:07 +0800

    fixes

--- src/view/panel/DeveloperWindow.ts
+++ src/view/panel/DeveloperWindow.ts
@@ -5,9 +5,11 @@ module game {
         public constructor() {
             super();
             this.skinName = "DeveloperWindow";
+            this.appVersion = platform.appVersion;
             this.addEventListener(eui.UIEvent.CREATION_COMPLETE, this.createCompleteEvent, this);
         }
-
+        
+        public appVersion: string;
         public createCompleteEvent(event: eui.UIEvent): void {
             this.height = this.stage.stageHeight;
             this.removeEventListener(eui.UIEvent.CREATION_COMPLETE, this.createCompleteEvent, this);
