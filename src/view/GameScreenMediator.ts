12f26677a4bec800f67f15742e8e41d7a43ac401
chenlunsen <563575103@qq.com>
2018-11-12 15:52:22 +0800

    fixes

--- src/view/GameScreenMediator.ts
+++ src/view/GameScreenMediator.ts
@@ -138,6 +138,7 @@ module game {
                     else {
                         // platform.showModal("体力不足，不能进入下一章", false);
                         this.sendNotification(SceneCommand.SHOW_POPUP, "体力不足，不能进入下一章");
+                        this.canGoNext = false;
                         return;
                     }
                 }
@@ -159,6 +160,7 @@ module game {
                     this.gameScreen.no_btnmanage.visible = false;
                     this.gameScreen.btnManage.visible = true;
                     this.sendNotification(GameScreenMediator.manage_show);
+                    this.canGoNext = true;
                 }
                 else if (this.proxy.playerInfo.time) {
                     this.gameScreen.showTransition = true;
@@ -167,11 +169,11 @@ module game {
                         this.gameScreen.showTransition = false;
                         this.btnManageClick();
                     }, this, 1000);
+                    this.canGoNext = true;
                 }
                 else {
                     this.showNext();
                 }
-                this.canGoNext = true;
             }
             else {
                 this.gameScreen.scrollGroup.bottom = 20;
